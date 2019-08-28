476253fb0268c4445ce56cb10c582bae9b97da37
Mythologos <bothwells@xavier.edu>
2019-08-28 14:17:59 -0400

    Added documentation throughout. Removed .idea folder with the .gitignore.

--- smorgasDB.py
+++ smorgasDB.py
@@ -21,6 +21,11 @@ Session = sessionmaker(bind=engine)
 
 
 def session_method(decorated_function):
+    """
+    Decorator; automatically sets up and closes sessions for database connections.
+    :param decorated_function: a function that requires a session.
+    :return: a function with the built-in capability of opening and closing a session.
+    """
     @functools.wraps(decorated_function)
     def session_decorator(*args, **kwargs):
         method_session = Session()
@@ -31,6 +36,9 @@ def session_method(decorated_function):
 
 
 class Quote(Base):
+    """
+    This class represents a quote stored from a server.
+    """
     __tablename__ = 'quotes'
 
     # Attributes:
@@ -50,6 +58,9 @@ class Quote(Base):
 
 
 class Guild(Base):
+    """
+    This class represents a Discord server, called a Guild.
+    """
     __tablename__ = 'guilds'
 
     # Attributes:
@@ -67,12 +78,24 @@ class Guild(Base):
 
 
 def reset_database():
+    """
+    This method resets the database down to the structure based upon the classes described above.
+    :return: None.
+    """
     Base.metadata.drop_all()
     Base.metadata.create_all()
 
 
 @session_method
 def create_quote_with(method_session, g_id, quote, auth=None):
+    """
+    This method creates and stores a Quote in the database.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :param quote: the text of the quotation (String).
+    :param auth: the author of the quotation (String).
+    :return: None.
+    """
     new_quote = Quote(author=auth, guild_id=g_id, text=quote)
     method_session.add(new_quote)
     method_session.commit()
@@ -80,18 +103,37 @@ def create_quote_with(method_session, g_id, quote, auth=None):
 
 @session_method
 def get_random_quote_by(method_session, g_id, q_number):
+    """
+    This method retrieves a random quote from a given server from the database.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :param q_number: a random number less than the maximum number of quotes that a server has.
+    :return: a Quote's author and text, in that order, in a Tuple.
+    """
     quote = method_session.query(Quote.author, Quote.text).filter_by(guild_id=g_id)[q_number]
     return quote
 
 
 @session_method
 def count_quotes(method_session, g_id):
+    """
+    This method counts the number of quotes that a Guild has stored in the database.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :return: the number of quotes which belong to a given Guild (an Integer).
+    """
     count = method_session.query(Quote).filter_by(guild_id=g_id).count()
     return count
 
 
 @session_method
 def has_assigned_channel_by(method_session, g_id):
+    """
+    This method determines whether a Guild has an assigned channel for embedding quotes.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :return: a Boolean of whether a Guild has a channel assigned to it according to the database.
+    """
     assigned_channel = method_session.query(Guild.quotation_channel_id).filter_by(guild_id=g_id).first()
     has_assigned_channel = assigned_channel is not None
     return has_assigned_channel
@@ -99,12 +141,25 @@ def has_assigned_channel_by(method_session, g_id):
 
 @session_method
 def get_assigned_channel_by(method_session, g_id):
+    """
+    This method retrieves the assigned channel for a given Guild.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :return: a channel ID (Integer).
+    """
     assigned_channel = method_session.query(Guild.quotation_channel_id).filter_by(guild_id=g_id).first()
     return assigned_channel[0]
 
 
 @session_method
 def create_guild_with(method_session, g_id, c_id):
+    """
+    This method creates a Guild and stores it in the database.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :param c_id: a Discord Channel ID (Integer).
+    :return: None.
+    """
     new_guild = Guild(guild_id=g_id, quotation_channel_id=c_id)
     method_session.add(new_guild)
     method_session.commit()
@@ -112,6 +167,13 @@ def create_guild_with(method_session, g_id, c_id):
 
 @session_method
 def update_assigned_channel(method_session, g_id, c_id):
+    """
+    This method retrieves a Guild and updates its assigned channel.
+    :param method_session: a Session database connection.
+    :param g_id: a Discord Guild ID (Integer).
+    :param c_id: a Discord Channel ID (Integer).
+    :return: None.
+    """
     method_session.query(Guild)\
                   .filter_by(guild_id=g_id)\
                   .update({"quotation_channel_id": c_id})
--- smorgasbord.py
+++ smorgasbord.py
@@ -12,6 +12,12 @@ reset_database = True
 
 @smorg.event
 async def on_ready():
+    """
+    This method is called Smorg is connected to Discord.
+    It assures that it has stored Guild information for every Guild that it is a part of.
+    This is important because it must know in which channel it should perform some of its functions.
+    :return: None.
+    """
     on_ready_message = "Hello! Smorg is online! To view commands, please type '.support'."
     if reset_database:
         smorgasDB.reset_database()
@@ -28,12 +34,20 @@ async def on_ready():
                 smorgasDB.create_guild_with(guild.id, guild.text_channels[0].id)
                 await smorg.get_channel(guild.text_channels[0].id).send(on_ready_message)
             else:
-                print("TODO: error behavior for Guild with no text channels.")
+                # TODO: make the below better...
+                print("Error! There are no text channels.")
 
 
 @smorg.command(description='This command embeds a quote. It takes a quote (in quotation marks) ' +
                            'and, optionally, an author as arguments.')
 async def quote(ctx, quotation='', author='An Anonymous Intellectual'):
+    """
+    This method receives a quotation and embeds it in its assigned chat.
+    :param ctx: The context from which the quotation came.
+    :param quotation: a quote to be embedded (a String).
+    :param author: the name of the author of the quote (a String).
+    :return: None.
+    """
     assigned_channel_id = smorgasDB.get_assigned_channel_by(ctx.message.channel.guild.id)
     current_channel = smorg.get_channel(assigned_channel_id)
     quotation = quotation.strip()
@@ -52,6 +66,14 @@ async def quote(ctx, quotation='', author='An Anonymous Intellectual'):
 @smorg.command(description='This command embeds a quote and stores it for posterity\'s sake. ' +
                            'It takes a quote (in quotation marks) and, optionally, an author as arguments.')
 async def sanctify(ctx, quotation='', author='An Unknowable Demigod'):
+    """
+    This method receives a quotation and embeds it in its assigned chat.
+    It also stores this information in the database.
+    :param ctx: The context from which the quotation came.
+    :param quotation: a quote to be embedded (a String).
+    :param author: the name of the author of the quote (a String).
+    :return: None.
+    """
     current_guild_id = ctx.message.channel.guild.id
     assigned_channel_id = smorgasDB.get_assigned_channel_by(current_guild_id)
     current_channel = smorg.get_channel(assigned_channel_id)
@@ -74,6 +96,12 @@ async def sanctify(ctx, quotation='', author='An Unknowable Demigod'):
 
 @smorg.command(description='This command retrieves and displays a random stored quote.')
 async def yoink(ctx):
+    """
+    This method retrieves a random Quote formed by the calling Guild in the database.
+    It also lets the user know whether a Quote even exists.
+    :param ctx: The context from which the quotation came.
+    :return: None.
+    """
     current_guild_id = ctx.message.channel.guild.id
     maximum = smorgasDB.count_quotes(current_guild_id) - 1
     if maximum >= 0:
@@ -95,6 +123,15 @@ async def yoink(ctx):
                            'It takes a channel\'s name and, if the channel has the same name as other channels, ' +
                            'the number of the instance of the channel as an argument.')
 async def govern(ctx, channel_name='', index=1):
+    """
+    This method allows users to alter the chat in which Smorg embeds its quotes.
+    :param ctx: The context from which the quotation came.
+    :param channel_name: The name of the channel which the user wants to designate as Smorg's Quote domain.
+    :param index: Given that multiple channels could have the same name,
+    this indicates the instance at which the chosen channel appears.
+    Smorg assumes that this is derived from counting Discord channels from top to bottom.
+    :return: None.
+    """
     govern_message = 'Congrats! You have successfully changed where I engrave your greatest sayings.'
     current_guild = ctx.message.channel.guild
     valid_channels = [channel for channel in current_guild.text_channels if channel.name == channel_name]
@@ -111,6 +148,11 @@ async def govern(ctx, channel_name='', index=1):
 
 @smorg.command(description='This command retrieves the menu below shown here.')
 async def support(ctx):
+    """
+    This method displays Smorg's help menu.
+    :param ctx: The context from which the quotation came.
+    :return: None.
+    """
     support_embed = discord.Embed(title='Smorg Support',
                                   description='This bot supports the following commands:',
                                   color=0x20409A)
