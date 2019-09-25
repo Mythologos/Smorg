4fb8b8f77c1e961010be5c867df2404b202cc04e
Mythologos <bothwells@xavier.edu>
2019-09-25 16:49:01 -0400

    Changed to UTC to match modern standards. Tried to add every valid time zone abbreviation that I could find and shortcuts to ambiguously-named ones.

--- Cogs/Helpers/time_zone.py
+++ Cogs/Helpers/time_zone.py
@@ -1,34 +1,46 @@
 from aenum import Enum
 
 
-# note: names dictate military time zone name
+# note: names dictate military time zone name, if it exists
+# continue here: https://www.timeanddate.com/time/zones/ ... at G
 class TimeZone(Enum, init='value aliases'):
-    A = (1, ('GMT+1', 'CET', 'WAT', 'BST', 'IST', 'WEDT', 'WEST', 'MEZ'))
-    B = (2, ('GMT+2', 'EET', 'USZ1', 'CEDT', 'CEST', 'MEST', 'MESZ'))
-    C = (3, ('GMT+3', 'MSK', 'EAT', 'EEDT', 'EEST'))
-    D = (4, ('GMT+4', 'SMT', 'AMT', 'AZT', 'GET', 'MUT'))
-    E = (5, ('GMT+5', 'PKT', 'YEKT', 'MVT'))
-    F = (6, ('GMT+6', 'OMSK', 'BST'))
-    G = (7, ('GMT+7', 'CXT', 'KRAT'))
-    H = (8, ('GMT+8', 'CST', 'CST+8', 'IRKT', 'AWST', 'WST', 'WADT'))
-    I = (9, ('GMT+9', 'JST', 'YAKT'))
-    K = (10, ('GMT+10', 'EAST', 'EST', 'EST+10', 'VLAT'))
-    L = (11, ('GMT+11', 'SAKT', 'EADT', 'EST', 'EST+11'))
-    M = (12, ('GMT+12', 'IDLE', 'NZST', 'NZT', 'MAGT'))
-    MSTAR = (13, ('GMT+13', 'NZDT'))
-    N = (-1, ('GMT-1', 'WAT'))
-    O = (-2, ('GMT-2', 'AT'))
-    P = (-3, ('GMT-3', 'ART', 'ADT', 'HAA'))
-    Q = (-4, ('GMT-4', 'AST', 'EDT', 'HAE', 'HNA'))
-    R = (-5, ('GMT-5', 'EST', 'EST-5', 'CDT', 'HAC', 'HNE'))
-    S = (-6, ('GMT-6', 'CST', 'CST-6', 'MDT', 'HAR', 'HNC'))
-    T = (-7, ('GMT-7', 'MST', 'PDT', 'HAP', 'HNR'))
-    U = (-8, ('GMT-8', 'PST', 'AKDT', 'YDT', 'HADT', 'HAY', 'HNP'))
-    V = (-9, ('GMT-9', 'AKST', 'YMT', 'HDT', 'HNY'))
-    W = (-10, ('GMT-10', 'HST', 'HAST', 'AHST', 'CAT'))
-    X = (-11, ('GMT-11', 'NT'))
-    Y = (-12, ('GMT-12', 'IDLW'))
-    Z = (0, ('GMT', 'GMT-0', 'GMT+0'))
+    A = (1, ('UTC+1', 'GMT+1', 'CET', 'WAT', 'BST', 'BST+1', 'BDT', 'BDST', 'IST', 'WEDT', 'WEST',
+             'MEZ', 'ECT', 'ECT+1'))
+    B = (2, ('UTC+2', 'GMT+2', 'EET', 'USZ1', 'CEDT', 'CEST', 'MEST', 'MESZ', 'CAT', 'ECST', 'OEZ'))
+    C = (3, ('UTC+3', 'GMT+3', 'MSK', 'EAT', 'EEDT', 'EEST', 'AST', 'AST+3', 'OESZ', 'FET'))
+    D = (4, ('UTC+4', 'GMT+4', 'SMT', 'AMT', 'AMT+4', 'AZT', 'GET', 'MUT', 'ADT', 'ADT+4'))
+    E = (5, ('UTC+5', 'GMT+5', 'PKT', 'YEKT', 'MVT', 'AMST', 'AMST+5', 'AQTT', 'AZST'))
+    F = (6, ('UTC+6', 'GMT+6', 'OMSK', 'BST', 'BST+6', 'ALMT', 'BTT'))
+    G = (7, ('UTC+7', 'GMT+7', 'CXT', 'KRAT', 'DAVT'))
+    H = (8, ('UTC+8', 'GMT+8', 'CST', 'CST+8', 'IRKT', 'AWST', 'WST', 'WADT', 'BNT', 'CAST',
+             'CHOT'))
+    I = (9, ('UTC+9', 'GMT+9', 'JST', 'YAKT', 'AWDT', 'CHOST'))
+    K = (10, ('UTC+10', 'GMT+10', 'EAST', 'EAST+10', 'EST', 'EST+10', 'VLAT', 'AEST', 'AET', 'AET+10',
+              'CHUT', 'ChST', 'GST', 'DDUT'))
+    L = (11, ('UTC+11', 'GMT+11', 'SAKT', 'EADT', 'EST', 'EST+11', 'AEDT', 'AET', 'AET+11', 'BST',
+              'BST+11'))
+    M = (12, ('UTC+12', 'GMT+12', 'IDLE', 'NZST', 'NZT', 'MAGT', 'ANAST', 'ANAT', 'FJT'))
+    MSTAR = (13, ('UTC+13', 'GMT+13', 'NZDT', 'FJST'))
+    EXTRA = (14, ('UTC+14', ))
+    N = (-1, ('UTC-1', 'GMT-1', 'WAT', 'AZOT', 'CVT', 'EGT'))
+    O = (-2, ('UTC-2', 'GMT-2', 'AT', 'BRST', 'FNT'))
+    P = (-3, ('UTC-3', 'GMT-3', 'ART', 'ADT', 'ADT-3', 'HAA', 'AMST', 'AMST-3', 'AT', 'AT-3', 'BRT',
+              'CLST', 'CLST-3', 'CLDT', 'FKST'))
+    Q = (-4, ('UTC-4', 'GMT-4', 'AST', 'AST-4', 'EDT', 'EDST', 'HAE', 'HNA', 'AMT', 'AMT-4', 'AT',
+              'AT-4', 'BOT', 'CDT', 'CDT-4', 'CIDST', 'CLT', 'CT', 'CT-4', 'CLST', 'CLST-4',
+              'NAEDT', 'FKT'))
+    R = (-5, ('UTC-5', 'GMT-5', 'EST', 'EST-5', 'CDT', 'CDT-5', 'CDST', 'NACDT', 'HAC', 'HNE', 'ACT',
+              'CIST', 'CIT', 'COT', 'CST', 'CST-5', 'CT', 'CT-5', 'EASST', 'ECT', 'ECT-5',
+              'ET', 'NAEST'))
+    S = (-6, ('UTC-6', 'GMT-6', 'CST', 'CST-6', 'CT', 'CT-6', 'NACST' 'MDT', 'HAR', 'HNC', 'EAST',
+              'EAST+10'))
+    T = (-7, ('UTC-7', 'GMT-7', 'MST', 'PDT', 'HAP', 'HNR'))
+    U = (-8, ('UTC-8', 'GMT-8', 'PST', 'AKDT', 'YDT', 'HADT', 'HAY', 'HNP'))
+    V = (-9, ('UTC-9', 'GMT-9', 'AKST', 'YMT', 'HDT', 'HNY'))
+    W = (-10, ('UTC-10', 'GMT-10', 'HST', 'HAST', 'AHST', 'CAT', 'CKT'))
+    X = (-11, ('UTC-11', 'GMT-11', 'NT'))
+    Y = (-12, ('UTC-12', 'GMT-12', 'IDLW', 'AoE'))
+    Z = (0, ('UTC', 'UTC+0', 'GMT', 'GMT+0', 'AZOST', 'EGST'))
 
     def __lt__(self, other):
         if isinstance(other, int) or isinstance(other, float) or isinstance(other, complex):
