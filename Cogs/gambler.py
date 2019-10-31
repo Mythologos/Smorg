3ccf30d7c84706b778599de4bbb3e4c3ef0fc7d7
Stephen Bothwell <bothwells.xu@gmail.com>
2019-10-30 21:02:26 -0400

    Added new Cog in preparation for future feature.

---
+++ Cogs/gambler.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+
+
+class Gambler(commands.Cog):
+    ...
+    # TODO: define class, set up roll types and syntax (regular, blind)
+    # Current Plans: regular dice (assume d6 as default). Allow for xdy rolls; build in possibility for keep / drop.
+
