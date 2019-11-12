cfff1d572ba5d5ba1c9533a6f1dad08444862391
Stephen Bothwell <bothwells.xu@gmail.com>
2019-11-12 12:24:46 -0500

    Added croupier to hold luck-based enumerations. Changed hazard to send roll results to n recipients and send the result to the user automatically. Added imperil, which sends roll results to n recipients not including (and actively excluding) the user. Split up handle_roll function into itself and a send_roll function. Changed name of chance_message to gamble_message to be more general.

---
+++ Cogs/Helpers/croupier.py
@@ -0,0 +1,35 @@
+from aenum import Enum
+
+
+class RollComponent(Enum):
+    NUMBER_OF_DICE = 0
+    DIE_SIZE = 1
+    DROP_KEEP_VALUE = 2
+    EXPLOSION_VALUE = 3
+    OVERALL_MODIFIER = 4
+    CHALLENGE_VALUE = 5
+
+
+class RollParseResult(Enum):
+    NUMBER_OF_DICE = 0
+    DIE_SIZE = 1
+    DROP_KEEP_VALUE = 2
+    DROP_KEEP_BOOLEAN = 3
+    EXPLOSION_BOOLEAN = 4
+    OVERALL_MODIFIER = 5
+    CHALLENGE_VALUE = 6
+    CHALLENGE_INDICATOR = 7
+
+
+class RollResult(Enum):
+    FINAL_RESULT = 0
+    UNSORTED_RESULTS = 1
+    INDIVIDUAL_RESULTS = 2
+
+
+# Note: challenge dice systems use > and < but mean >= and <= generally,
+# So this class uses the below in that way.
+class ComparisonIndicator(Enum):
+    LESS_THAN = -1
+    NO_COMPARISON = 0
+    GREATER_THAN = 1
--- Cogs/gambler.py
+++ Cogs/gambler.py
@@ -1,49 +1,18 @@
 # TODO: overall documentation
+# TODO: deck of cards; order of operations in modifiers.
+# TODO: warnings / safeguards against generating a message longer than 2000 characters.
+# TODO: handle case of vs. negative challenge dice.
+# TODO: divide up some of these functions; some are getting decently long.
 
 import discord
 from discord.ext import commands
 from smorgasDB import Guild
 from .Helpers.disambiguator import Disambiguator
+from .Helpers.croupier import RollComponent, RollParseResult, RollResult, ComparisonIndicator
 from random import randint
 from copy import deepcopy
 from collections import namedtuple
-from aenum import Enum
 import re
-
-
-# TODO: move these to a helper class, or separate ones
-class RollComponent(Enum):
-    NUMBER_OF_DICE = 0
-    DIE_SIZE = 1
-    DROP_KEEP_VALUE = 2
-    EXPLOSION_VALUE = 3
-    OVERALL_MODIFIER = 4
-    CHALLENGE_VALUE = 5
-
-
-class RollParseResult(Enum):
-    NUMBER_OF_DICE = 0
-    DIE_SIZE = 1
-    DROP_KEEP_VALUE = 2
-    DROP_KEEP_BOOLEAN = 3
-    EXPLOSION_BOOLEAN = 4
-    OVERALL_MODIFIER = 5
-    CHALLENGE_VALUE = 6
-    CHALLENGE_INDICATOR = 7
-
-
-class RollResult(Enum):
-    FINAL_RESULT = 0
-    UNSORTED_RESULTS = 1
-    INDIVIDUAL_RESULTS = 2
-
-
-# Note: challenge dice systems use > and < but mean >= and <= generally,
-# So I use the below in that way.
-class ComparisonIndicator(Enum):
-    LESS_THAN = -1
-    NO_COMPARISON = 0
-    GREATER_THAN = 1
 
 
 class Gambler(commands.Cog, Disambiguator):
@@ -59,43 +28,74 @@ class Gambler(commands.Cog, Disambiguator):
                                   'The default x value is 1. The default y value is 6. The others default to 0. ' +
                                   'Quoted, a description of what the roll was for may be included afterward. ' +
                                   'The result is posted either in a set gamble channel or where the die was rolled.')
-    async def chance(self, ctx, roll, description='To Contend with Lady Luck'):
+    async def chance(self, ctx, roll: str, description: str = 'To Contend with Lady Luck'):
         gamble_channel_id: int = Guild.get_gamble_channel_by(ctx.guild.id)
         current_channel = self.bot.get_channel(gamble_channel_id) if self.bot.get_channel(gamble_channel_id) \
             else ctx.message.channel
-        await self.handle_roll(ctx, current_channel, roll, description)
+        roll_results: tuple = await self.handle_roll(ctx, roll)
+        await self.send_roll(ctx, roll, roll_results[0], roll_results[1], current_channel, description)
 
-    @commands.command(description='This command gets Smorg to blindly roll one die or multiple dice. ' +
-                                  'It currently can handle rolls of the form xdy(k/d)z(!)(+/-)a(>/<)b, '
-                                  'where x, y, z, and b are nonnegative integers, ' +
-                                  'and where a is an integer. Regular dice, drop-keep syntax, ' +
-                                  'exploding dice, challenge dice, and combinations of these are all supported. ' +
-                                  'Only the the first "d" is required to perform a roll. ' +
-                                  'The default x value is 1. The default y value is 6. The others default to 0. ' +
-                                  'Quoted, a recipient\'s username or Guild nickname ' +
-                                  'must be given to DM the roll\'s result only to that person. ' +
-                                  'Quoted afterward, a description of what the roll was for may also be included.')
-    async def hazard(self, ctx, roll, recipient, description='To Contend with Lady Luck'):
+    @commands.command(description='This command gets Smorg to roll one die or multiple dice and ' +
+                                  'to send the result to one or more recipients. ' +
+                                  'These recipients are the second item to be typed in the command, ' +
+                                  'presented in quotes as usernames or server nicknames and delimited by semicolons. ' +
+                                  'The user is automatically listed as a recipient. ' +
+                                  'See the documentation for "chance" for a description of the remaining syntax.')
+    async def hazard(self, ctx, roll: str, recipients: str = None, description: str = 'To Contend with Lady Luck'):
+        starting_chosen_recipients: list = [ctx.message.author]
+        chosen_recipients: list = await self.get_recipients(ctx, recipients, starting_chosen_recipients) if recipients \
+            else starting_chosen_recipients
+        while chosen_recipients.count(ctx.message.author) > 1:
+            chosen_recipients.remove(ctx.message.author)
+        await self.inform_recipients(ctx, roll, chosen_recipients, description)
+
+    @commands.command(description='This command gets Smorg to roll one die or multiple dice and ' +
+                                  'to send the result to one or more recipients that do not include the user. ' +
+                                  'These recipients are the second item to be typed in the command, ' +
+                                  'presented in quotes as usernames or server nicknames and delimited by semicolons. ' +
+                                  'See the documentation for "chance" for a description of the remaining syntax.')
+    async def imperil(self, ctx, roll: str, recipients: str = None, description: str = 'To Contend with Lady Luck'):
+        chosen_recipients: list = await self.get_recipients(ctx, recipients) if recipients \
+            else await ctx.send("Error: no recipients were given for this roll.")
+        if ctx.message.author in chosen_recipients:
+            await ctx.send("Error: the sender is not allowed to be a recipient for this roll.")
+        else:
+            await self.inform_recipients(ctx, roll, chosen_recipients, description)
+
+    async def get_recipients(self, ctx, recipients: str, chosen_recipients: list = None) -> list:
+        if chosen_recipients is None:
+            chosen_recipients = []
+
         ReducedMember = namedtuple('ReducedMember', 'nickname username id')
         guild_members: list = list(map(lambda member: ReducedMember(member.nick, member.name, member.id),
                                        ctx.guild.members))
-        recipient_options: list = []
-        for reduced_member in guild_members:
-            if recipient in [reduced_member.nickname, reduced_member.username]:
-                recipient_options.append(reduced_member)
-        if len(recipient_options) == 0:
-            await ctx.send("Error: no individual matches the recipient name or nickname given. Please try again.")
-        else:
-            chosen_recipient_index: int = await Disambiguator.disambiguate(self.bot, ctx, recipient_options)
-            chosen_recipient = self.bot.get_user(recipient_options[chosen_recipient_index].id)
+        for recipient in recipients.split(';'):
+            recipient = recipient.strip()
+            found_recipients: list = []
+            for reduced_member in guild_members:
+                if recipient in [reduced_member.nickname, reduced_member.username]:
+                    found_recipients.append(reduced_member)
+            else:
+                if len(found_recipients) == 0:
+                    await ctx.send("Error: no individual matches the recipient name or nickname given for " +
+                                   recipient + "." + "Please try again.")
+                else:
+                    chosen_recipient_index: int = await Disambiguator.disambiguate(self.bot, ctx, found_recipients)
+                    chosen_recipient = self.bot.get_user(found_recipients[chosen_recipient_index].id)
+                    chosen_recipients.append(chosen_recipient)
+        return chosen_recipients
+
+    async def inform_recipients(self, ctx, roll: str, chosen_recipients: list, description: str):
+        roll_results: tuple = await self.handle_roll(ctx, roll)
+        for chosen_recipient in chosen_recipients:
             if not chosen_recipient.dm_channel:
                 await self.bot.get_user(chosen_recipient.id).create_dm()
             recipient_dm_channel = chosen_recipient.dm_channel
-            await self.handle_roll(ctx, recipient_dm_channel, roll, description)
+            await self.send_roll(ctx, roll, roll_results[0], roll_results[1],
+                                 recipient_dm_channel, description)
 
-    async def handle_roll(self, ctx, destination_channel, roll, description):
+    async def handle_roll(self, ctx, roll: str):
         roll: str = roll.strip()
-        description: str = description.strip()
         roll_parse_result: list = self.parse_roll(roll)
         result_errors: str = ''
         for result in roll_parse_result:
@@ -104,20 +104,23 @@ class Gambler(commands.Cog, Disambiguator):
         if result_errors:
             await ctx.send(result_errors)
         else:
-            roll_result: tuple = self.evaluate_roll(roll_parse_result)
-            raw_result_message: str = self.format_roll_result(roll_result[RollResult.UNSORTED_RESULTS.value])
-            end_result_message: str = self.format_roll_result(roll_result[RollResult.INDIVIDUAL_RESULTS.value])
-            modifier_message: str = " + " + str(roll_parse_result[RollParseResult.OVERALL_MODIFIER.value]) \
-                if roll_parse_result[RollParseResult.OVERALL_MODIFIER.value] != 0 else ""
-            chance_message = ctx.message.author.mention + "\n" + \
-                "Roll: " + roll + "\n" + \
-                "Reason: " + description + "\n" + \
-                "Raw Result: " + "(" + raw_result_message + ")" + modifier_message + "\n" + \
-                "End Result: " + "(" + end_result_message + ")" + modifier_message + \
-                " = " + str(roll_result[RollResult.FINAL_RESULT.value])
-            if roll_parse_result[RollParseResult.CHALLENGE_INDICATOR.value]:
-                chance_message += " Successes"
-            await destination_channel.send(chance_message)
+            return roll_parse_result, self.evaluate_roll(roll_parse_result)
+
+    async def send_roll(self, ctx, roll, roll_parse_result, roll_result, destination_channel, description):
+        description: str = description.strip()
+        raw_result_message: str = self.format_roll_result(roll_result[RollResult.UNSORTED_RESULTS.value])
+        end_result_message: str = self.format_roll_result(roll_result[RollResult.INDIVIDUAL_RESULTS.value])
+        modifier_message: str = " + " + str(roll_parse_result[RollParseResult.OVERALL_MODIFIER.value]) \
+            if roll_parse_result[RollParseResult.OVERALL_MODIFIER.value] != 0 else ""
+        gamble_message = ctx.message.author.mention + "\n" + \
+                         "Roll: " + roll + "\n" + \
+                         "Reason: " + description + "\n" + \
+                         "Raw Result: " + "(" + raw_result_message + ")" + modifier_message + "\n" + \
+                         "End Result: " + "(" + end_result_message + ")" + modifier_message + \
+                         " = " + str(roll_result[RollResult.FINAL_RESULT.value])
+        if roll_parse_result[RollParseResult.CHALLENGE_INDICATOR.value]:
+            gamble_message += " Successes"
+        await destination_channel.send(gamble_message)
 
     def parse_roll(self, roll: str) -> list:
         roll_regex = re.compile('(\d*)[dD](\d*)([dkDK][\d]+)?([!])?([+-]\d+)?([><]\d+)?')
@@ -175,7 +178,7 @@ class Gambler(commands.Cog, Disambiguator):
             return roll_list
 
     @staticmethod
-    def evaluate_roll(roll_parse_result):
+    def evaluate_roll(roll_parse_result: list) -> tuple:
         individual_results: list = []
         roll_index: int = 0
         explosion_count: int = 0
@@ -213,7 +216,7 @@ class Gambler(commands.Cog, Disambiguator):
         return roll_evaluation
 
     @staticmethod
-    def format_roll_result(roll_sequence):
+    def format_roll_result(roll_sequence: list):
         result_message: str = ''
         for result in roll_sequence:
             if result_message:
@@ -223,13 +226,9 @@ class Gambler(commands.Cog, Disambiguator):
         return result_message
 
     @staticmethod
-    def check_basic_roll_component(default_quantity, item, character_range_start=0, character_range_end=None):
+    def check_basic_roll_component(default_quantity: int, item: str, character_range_start: int = 0,
+                                   character_range_end: int = None):
         roll_component_value: int = default_quantity
         if item and item[character_range_start:character_range_end].isalnum():
             roll_component_value = int(item[character_range_start:character_range_end])
         return roll_component_value
-
-    # TODO: deck of cards; gmroll dice; n-recipient dice rolls; order of operations in modifiers.
-    # TODO: warnings / safeguards against generating a message longer than 2000 characters.
-    # TODO: handle case of vs. negative challenge dice.
-    # TODO: divide up some of these functions; some are getting decently long.
